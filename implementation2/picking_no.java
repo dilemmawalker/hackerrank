a324f564b3e3018b72924799268bfd54ea9264db
yash handa <yashhanda500@gmail.com>
2020-11-17 19:05:48 +0530

    update

--- implementation2/picking_no.java
+++ implementation2/picking_no.java
@@ -1,23 +1,30 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.io.*;
 public class picking_no{
-    public static Scanner scn=new Scanner(System.in);
+    public static Scanner scn=new Scanner (System.in);
     public static void main(String[]args){
-        int n=scn.nextInt();
-        int []arr=new int[n];
-        for(int i=0;i<n-1;i++){
-           int a=scn.nextInt();
-           arr[i]=a;
+            int n=scn.nextInt();
+            int[]arr=new int[n];
+            for(int i=0;i<n;i++)
+            arr[i]=scn.nextInt();
+
+           int a= solve(arr,0,1,0);
+           System.out.println(a);
+    }
+    public static int solve(int[]arr,int idx,int count,int prev){
+        if(idx==arr.length)
+        {
+            return count;
         }
-        int max=0;
-        for(int i=0;i<n;i++){
-            int c=1;
-            for(int j=i+1;j<n;j++){
-                if(arr[i]==arr[j] || arr[i]-arr[j]==1 || arr[j]-arr[i]==1)
-                c++;
-            }
-            max=Math.max(c,max);
+
+        int c=0;
+        if((arr[idx]-prev)==0)
+        c=Math.max(solve(arr,idx+1,count+1,prev),c);
+        else{
+        c=Math.max(solve(arr,idx+1,count,prev),c);
+        if(count==0 || (arr[idx]-prev)==1 ||(arr[idx]-prev)==-1)
+        c=Math.max(solve(arr,idx+1,count+1,arr[idx]),c);
         }
-        System.out.println(max);
+        return c;
     }
 }
