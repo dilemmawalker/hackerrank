f30f52ab9355325251694b0e05dc4cbc823e67f9
yash handa <yashhanda500@gmail.com>
2020-11-18 21:35:41 +0530

    update

--- implementation2/picking_no.java
+++ implementation2/picking_no.java
@@ -5,27 +5,35 @@ public class picking_no{
     public static void main(String[]args){
             int n=scn.nextInt();
             int[]arr=new int[n];
-            int[]dp=new int[n+1];
-
-            for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++)
             arr[i]=scn.nextInt();
-            dp[i]=-10;
-            }
-            dp[n]=-10;
-           int a= solve(arr,0,0,0,dp);
+
+           int a= solve(arr,0,0,1000,1000);
            System.out.println(a);
     }
-    public static int solve(int[]arr,int idx,int count,int prev,int[]dp){
+    public static int solve(int[]arr,int idx,int count,int min,int max){
         if(idx==arr.length)
         {
             return count;
         }
-        if(dp[idx]!=-10)
-        return dp[idx];
+
         int c=0;
-        c=Math.max(solve(arr,idx+1,count,prev,dp),c);
-        if(count==0 || (arr[idx]-prev)==0 || (arr[idx]-prev)==1 ||(arr[idx]-prev)==-1)
-        c=Math.max(solve(arr,idx+1,count+1,arr[idx],dp),c);
-        return dp[idx]=c;
+        // if((arr[idx]-prev)==0 && count!=0)
+        // c=Math.max(solve(arr,idx+1,count+1,prev),c);
+        // else{
+        c=Math.max(solve(arr,idx+1,count,min,max),c);
+        if(count==0 || ((arr[idx]-min)==1 ||(min-arr[idx])==1||(min-arr[idx]==0)) && 
+        ((arr[idx]-max)==1 ||(max-arr[idx])==1)|| (max-arr[idx])==0){
+            int m1,m2;
+            m1=min;
+            m2=max;
+            min=Math.min(min,arr[idx]);
+            max=Math.min(max,arr[idx]);
+            c=Math.max(solve(arr,idx+1,count+1,min,max),c);
+            min=m1;
+            max=m2;
+        }
+        // }
+        return c;
     }
 }
