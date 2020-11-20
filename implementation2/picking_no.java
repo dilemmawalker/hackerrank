0875740b39edd54232a75ea7022c61618b5f136c
yash handa <yashhanda500@gmail.com>
2020-11-20 22:59:46 +0530

    update

--- implementation2/picking_no.java
+++ implementation2/picking_no.java
@@ -7,21 +7,24 @@ public class picking_no{
             int[]arr=new int[n];
             for(int i=0;i<n;i++)
             arr[i]=scn.nextInt();
-
-           int a= solve(arr,0,0,1000,0);
+        int[]dp=new int[n+1];
+        for(int i=0;i<n;i++)
+        dp[i]=-1;
+           int a= solve(arr,0,0,1000,0,dp);
            System.out.println(a);
     }
-    public static int solve(int[]arr,int idx,int count,int min,int max){
+    public static int solve(int[]arr,int idx,int count,int min,int max,int[]dp){
         if(idx==arr.length)
         {
-            return count;
+            return 0;
         }
-
+        if(dp[idx]!=-1)
+        return dp[idx];
         int c=0;
         // if((arr[idx]-prev)==0 && count!=0)
         // c=Math.max(solve(arr,idx+1,count+1,prev),c);
         // else{
-        c=Math.max(solve(arr,idx+1,count,min,max),c);
+        c=Math.max(solve(arr,idx+1,count,min,max,dp),c);
         if(count==0 || ((((arr[idx]-min)==1) ||((min-arr[idx])==1)||((min-arr[idx]==0))) && 
         (((arr[idx]-max)==1) ||((max-arr[idx])==1)|| ((max-arr[idx])==0)))){
             int m1,m2;
@@ -29,11 +32,11 @@ public class picking_no{
             m2=max;
             min=Math.min(min,arr[idx]);
             max=Math.max(max,arr[idx]);
-            c=Math.max(solve(arr,idx+1,count+1,min,max),c);
+            c=Math.max(solve(arr,idx+1,count+1,min,max,dp)+1,c);
             min=m1;
             max=m2;
         }
         // }
-        return c;
+        return dp[idx]=c;
     }
 }
