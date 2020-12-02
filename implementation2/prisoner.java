968bb079b8bdec437e2dccee9c9771639fe61775
yash handa <yashhanda500@gmail.com>
2020-12-02 23:51:40 +0530

    update

--- implementation2/prisoner.java
+++ implementation2/prisoner.java
@@ -5,23 +5,18 @@ public class prisoner{
     public static void main(String[]args){
         int t=scn.nextInt();
         while(t-->0){
-            int n=scn.nextInt();
-            int m=scn.nextInt();
-            int s=scn.nextInt();
-            if(m>n){
-            m=(m%(n));
-            }
-            // System.out.println(m);
-            if(m==0)
-            s=s+1;
-            else
-            while(--m>0){
-                s+=1;
-            }
-            if(s<=n)
-            System.out.println(s);
-            else
-            System.out.println(s%n);
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+        int s=scn.nextInt();
+         m=m%n;
+        if(m==0)
+        m=n;
+
+        m+=s-1;
+        m=m%n;
+        if(m==0)
+        m=n;
+            System.out.println(m);
         }
     }
 }
