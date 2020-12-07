acce2dedc5b6464a3ab7c04fbaf944d55cd3b461
yash handa <yashhanda500@gmail.com>
2020-12-07 22:42:37 +0530

    update

---
+++ implementation3/circular.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+public class circular{
+    public static Scanner scn=new Scanner(System.in);
+    public static void main(String[]args){
+        int n=scn.nextInt();
+        int k=scn.nextInt();
+        int q=scn.nextInt();
+        int[]arr=new int[n];
+        for(int i=0;i<n;i++){
+            int a=scn.nextInt();
+            arr[i]=a;
+        }
+        for(int i=0;i<q;i++){
+            int a=scn.nextInt();
+            a+=k-1;
+            a%=n;
+            System.out.println(arr[a]);
+        }
+    }
+}
